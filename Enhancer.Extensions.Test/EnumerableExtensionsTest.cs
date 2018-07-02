6ade019cf0499350016f567d46c4a5d48faa2172
Ádám L. Juhász <jadaml@gmail.com>
2018-07-02 22:12:16 +0200

    Improved unit tests for EnumerableExtensions.

--- Enhancer.Extensions.Test/EnumerableExtensionsTest.cs
+++ Enhancer.Extensions.Test/EnumerableExtensionsTest.cs
@@ -16,78 +16,225 @@
  * along with Enhancer.Extensions.Test.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+using NSubstitute;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static NUnit.Framework.Assert;
+using static System.Linq.Enumerable;
+using static System.Math;
 
 namespace Enhancer.Extensions.Test
 {
     [TestFixture]
     public class EnumerableExtensionsTest
     {
-        private static ValueTuple<bool, int, int>[] _haveAtLeastSource = new ValueTuple<bool, int, int>[]
+        [ExcludeFromCodeCoverage]
+        private class TestCollection<T> : ICollection<T>
         {
-            (true, 3, 2),
-            (true, 3, 3),
-            (false, 3, 4),
-        };
-        private static ValueTuple<bool, int, int>[] _haveAtMostSource = new ValueTuple<bool, int, int>[]
+            public int Count { get; }
+
+            bool ICollection<T>.IsReadOnly => throw new NotImplementedException();
+
+            public TestCollection(int count)
+            {
+                Count = count;
+            }
+
+            void ICollection<T>.Add(T item)
+            {
+                throw new NotImplementedException();
+            }
+
+            void ICollection<T>.Clear()
+            {
+                throw new NotImplementedException();
+            }
+
+            bool ICollection<T>.Contains(T item)
+            {
+                throw new NotImplementedException();
+            }
+
+            void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            bool ICollection<T>.Remove(T item)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private static object[][] _haveAtLeastSource = new object[][]
         {
-            (false, 3, 2),
-            (true, 3, 3),
-            (true, 3, 4),
+            new object[] { true, 3, 2 },
+            new object[] { true, 3, 3 },
+            new object[] { false, 3, 4 },
         };
-        private static ValueTuple<bool, int, int>[] _haveExactlySource = new ValueTuple<bool, int, int>[]
+        private static object[][] _haveAtMostSource = new object[][]
         {
-            (false, 3, 2),
-            (true, 3, 3),
-            (false, 3, 4),
+            new object[] { false, 3, 2 },
+            new object[] { true, 3, 3 },
+            new object[] { true, 3, 4 },
         };
+        private static object[][] _haveExactlySource = new object[][]
+        {
+            new object[] { false, 3, 2 },
+            new object[] { true, 3, 3 },
+            new object[] { false, 3, 4 },
+        };
+
+        private static readonly Random rnd = Substitute.For<Random>();
+
+        [TestCase(0, 3, 1)]
+        [TestCase(0, 3, 4)]
+        [TestOf(typeof(EnumerableExtensions))]
+        public void IsAnyOfTest(int start, int count, int needle)
+        {
+            IEnumerable<int> collection = Range(start, count);
+
+            AreEqual(collection.Contains(needle), EnumerableExtensions.IsAnyOf(needle, collection));
+            AreEqual(collection.Contains(needle), EnumerableExtensions.IsAnyOf(needle, collection.ToArray()));
+        }
+
+        [TestCase(0, 3, 1)]
+        [TestCase(0, 3, 4)]
+        [TestOf(typeof(EnumerableExtensions))]
+        public void IsNeitherTest(int start, int count, int needle)
+        {
+            IEnumerable<int> collection = Range(start, count);
+
+            AreEqual(!collection.Contains(needle), EnumerableExtensions.IsNeitherOf(needle, collection));
+            AreEqual(!collection.Contains(needle), EnumerableExtensions.IsNeitherOf(needle, collection.ToArray()));
+        }
+
+        [Test(TestOf = typeof(EnumerableExtensions))]
+        public void HaveAtLeastError()
+        {
+            Throws<ArgumentNullException>(() => EnumerableExtensions.HaveAtLeast(null, 0));
+        }
 
         [TestOf(typeof(EnumerableExtensions))]
         [TestCaseSource(nameof(_haveAtLeastSource))]
-        public void HaveAtLeast(ValueTuple<bool, int, int> input)
+        public void HaveAtLeast(bool expect, int count, int value)
         {
-            if (input.Item1)
+            if (expect)
             {
-                IsTrue(new object[input.Item2].HaveAtLeast(input.Item3), "Count {0} failed", input.Item3);
+                IsTrue(new object[count].HaveAtLeast(value), "Count {0} failed", value);
+                IsTrue(new List<object>(new object[count]).HaveAtLeast(value), "Count {0} failed", value);
+                IsTrue(new TestCollection<object>(count).HaveAtLeast(value), "Count {0} failed", value);
+                IsTrue(Range(0, count).HaveAtLeast(value), "Count {0} failed", value);
             }
             else
             {
-                IsFalse(new object[input.Item2].HaveAtLeast(input.Item3), "Count {0} failed", input.Item3);
+                IsFalse(new object[count].HaveAtLeast(value), "Count {0} failed", value);
+                IsFalse(new List<object>(new object[count]).HaveAtLeast(value), "Count {0} failed", value);
+                IsFalse(new TestCollection<object>(count).HaveAtLeast(value), "Count {0} failed", value);
+                IsFalse(Range(0, count).HaveAtLeast(value), "Count {0} failed", value);
             }
+        }
+
+        [Test(TestOf = typeof(EnumerableExtensions))]
+        public void HaveAtMostError()
+        {
+            Throws<ArgumentNullException>(() => EnumerableExtensions.HaveAtMost(null, 0));
         }
 
         [TestOf(typeof(EnumerableExtensions))]
         [TestCaseSource(nameof(_haveAtMostSource))]
-        public void HaveAtMost(ValueTuple<bool, int, int> input)
+        public void HaveAtMost(bool expect, int count, int value)
         {
-            if (input.Item1)
+            if (expect)
             {
-                IsTrue(new object[input.Item2].HaveAtMost(input.Item3), "Count {0} failed", input.Item3);
+                IsTrue(new object[count].HaveAtMost(value), "Count {0} failed", value);
+                IsTrue(new List<object>(new object[count]).HaveAtMost(value), "Count {0} failed", value);
+                IsTrue(new TestCollection<object>(count).HaveAtMost(value), "Count {0} failed", value);
+                IsTrue(Range(0, count).HaveAtMost(value), "Count {0} failed", value);
             }
             else
             {
-                IsFalse(new object[input.Item2].HaveAtMost(input.Item3), "Count {0} failed", input.Item3);
+                IsFalse(new object[count].HaveAtMost(value), "Count {0} failed", value);
+                IsFalse(new List<object>(new object[count]).HaveAtMost(value), "Count {0} failed", value);
+                IsFalse(new TestCollection<object>(count).HaveAtMost(value), "Count {0} failed", value);
+                IsFalse(Range(0, count).HaveAtMost(value), "Count {0} failed", value);
             }
+        }
+
+        [Test(TestOf = typeof(EnumerableExtensions))]
+        public void HaveExactlyError()
+        {
+            Throws<ArgumentNullException>(() => EnumerableExtensions.HaveExactly(null, 0));
         }
 
         [TestOf(typeof(EnumerableExtensions))]
         [TestCaseSource(nameof(_haveExactlySource))]
-        public void HaveExactly(ValueTuple<bool, int, int> input)
+        public void HaveExactly(bool expect, int count, int value)
         {
-            if (input.Item1)
+            if (expect)
             {
-                IsTrue(new object[input.Item2].HaveExactly(input.Item3), "Count {0} failed", input.Item3);
+                IsTrue(new object[count].HaveExactly(value), "Count {0} failed", value);
+                IsTrue(new List<object>(new object[count]).HaveExactly(value), "Count {0} failed", value);
+                IsTrue(new TestCollection<object>(count).HaveExactly(value), "Count {0} failed", value);
+                IsTrue(Range(0, count).HaveExactly(value), "Count {0} failed", value);
             }
             else
             {
-                IsFalse(new object[input.Item2].HaveExactly(input.Item3), "Count {0} failed", input.Item3);
+                IsFalse(new object[count].HaveExactly(value), "Count {0} failed", value);
+                IsFalse(new List<object>(new object[count]).HaveExactly(value), "Count {0} failed", value);
+                IsFalse(new TestCollection<object>(count).HaveExactly(value), "Count {0} failed", value);
+                IsFalse(Range(0, count).HaveExactly(value), "Count {0} failed", value);
             }
+        }
+
+        [Test(TestOf = typeof(EnumerableExtensions))]
+        public void RandomElementError()
+        {
+            Throws<ArgumentNullException>(() => EnumerableExtensions.RandomElement<int>(null, rnd));
+            Throws<ArgumentNullException>(() => EnumerableExtensions.RandomElement(new int[0], null));
+            Throws<ArgumentOutOfRangeException>(() => EnumerableExtensions.RandomElement(new int[0], rnd, -1));
+            Throws<ArgumentException>(() => EnumerableExtensions.RandomElement(new int[2], rnd, 1, 2));
+        }
+
+        [Test(TestOf = typeof(EnumerableExtensions))]
+        public void RandomElement()
+        {
+            rnd.Next(Arg.Any<int>(), Arg.Any<int>()).Returns(x => (int)x[0] + Abs((int)x[0] - (int)x[1]) / 2);
+
+            AreEqual(2, EnumerableExtensions.RandomElement(Range(0, 4).ToArray(), rnd, 1));
+            AreEqual(2, EnumerableExtensions.RandomElement(Range(0, 5).ToArray(), rnd, 1, 3));
+        }
+
+        [Test(TestOf = typeof(EnumerableExtensions))]
+        public void SelectError()
+        {
+            Throws<ArgumentNullException>(() => EnumerableExtensions.Select(null, new int[0]));
+            Throws<ArgumentNullException>(() => EnumerableExtensions.Select<int>(rnd, null));
+            Throws<ArgumentOutOfRangeException>(() => EnumerableExtensions.Select(rnd, new int[0], -1));
+            Throws<ArgumentException>(() => EnumerableExtensions.Select(rnd, new int[2], 1, 2));
+        }
+
+        [Test(TestOf = typeof(EnumerableExtensions))]
+        public void Select()
+        {
+            rnd.Next(Arg.Any<int>(), Arg.Any<int>()).Returns(x => (int)x[0] + Abs((int)x[0] - (int)x[1]) / 2);
+
+            AreEqual(2, EnumerableExtensions.Select(rnd, Range(0, 4).ToArray(), 1));
+            AreEqual(2, EnumerableExtensions.Select(rnd, Range(0, 5).ToArray(), 1, 3));
         }
     }
 }
---
+++ Enhancer.Extensions.Test/TypeExtensionsTest.cs
@@ -0,0 +1,71 @@
+﻿/* Copyright (c) 2018, Ádám L. Juhász
+ *
+ * This file is part of Enhancer.Extensions.Test.
+ *
+ * Enhancer.Extensions.Test is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Enhancer.Extensions.Test is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Enhancer.Extensions.Test.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestType = System.Int32;
+using static NUnit.Framework.CollectionAssert;
+using static NUnit.Framework.Assert;
+
+namespace Enhancer.Extensions.Test
+{
+    [TestFixture]
+    public class TypeExtensionsTest
+    {
+        private class SpecialList : List<TestType>
+        {
+            public SpecialList() : base(0) { }
+        }
+
+        private static object[][] _types = new object[][]
+        {
+            new object[] { typeof(Guid),            null                    },
+            new object[] { typeof(Tuple<,>),        null                    },
+            new object[] { typeof(IComparable),     null                    },
+            new object[] { typeof(IComparable<>),   null                    },
+            new object[] { typeof(IList),           typeof(IList)           },
+            new object[] { typeof(IList<>),         typeof(IList<TestType>) },
+            new object[] { typeof(IList<TestType>), typeof(IList<TestType>) },
+            new object[] { typeof(List<>),          typeof(List<TestType>)  },
+            new object[] { typeof(List<TestType>),  typeof(List<TestType>)  },
+            new object[] { typeof(SpecialList),     typeof(SpecialList)     },
+        };
+
+        [Test(TestOf = typeof(TypeExtensions))]
+        public void GetGenericTypeError()
+        {
+            Throws<ArgumentNullException>(() => TypeExtensions.GetGenericType(null, typeof(IList<>)));
+            Throws<ArgumentNullException>(() => TypeExtensions.GetGenericType<List>(null, typeof(IList<>)));
+            Throws<ArgumentNullException>(() => TypeExtensions.GetGenericType(new object(), null));
+            Throws<ArgumentNullException>(() => TypeExtensions.GetGenericType(new List(), null));
+        }
+
+        [TestCaseSource(nameof(_types))]
+        [TestOf(typeof(TypeExtensions))]
+        public void GetGenericTypeTest(Type value, Type expect)
+        {
+            AreEqual(expect, TypeExtensions.GetGenericType(typeof(SpecialList), value));
+            AreEqual(expect, TypeExtensions.GetGenericType(new SpecialList(), value));
+        }
+    }
+}
